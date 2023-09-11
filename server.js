48642ecaab63e2f73a469323cb9ba3768d88c579
Sayeeduddin <iamsayeeduddin@outlook.com>
2023-09-11 10:28:14 +0530

    REST API

--- server.js
+++ server.js
@@ -34,9 +34,31 @@ const PORT = process.env.PORT || 5000;
 // Higher Order Function
 server.listen(PORT, () => console.log(`Server is Running on ${PORT}!`));
 
+// HTTP
+
 // Status
 // 1xx - Informational
 // 2xx - Successful
 // 3xx - Redirect
 // 4xx - User/Client Error
 // 5xx - Server Error
+
+// REST - REpresentational State Transfer
+// Client - Cache - Server - Database
+// 1 - Uniform Interface -
+// 2 - Client Server Decoupling -
+// 3 - Statelessness -
+// 4 - Cacheability - Cache -
+// 5 - Layered System Architecture - Route - Middlewares (Authentication, Multimedia, etcc,,,) - Database
+// 6 - Code On Demand - Optional -
+
+// JS (NOde JS & Express JS), Python, Java
+
+// REST APIs -> HTTP -> CRUD Operations
+// C -> Create Data -> POST
+// R -> Read Data -> GET
+// U -> Update Data -> PATCH / PUT
+// D -> Delete Data -> DELETE
+
+// Body -> Get Back Result -> Send The Data
+// Request Headers & Parameters ->
