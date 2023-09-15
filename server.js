8e2e3a187ebf156b52047c5eff7241c7af6f302e
Sayeeduddin <iamsayeeduddin@outlook.com>
2023-09-15 09:37:59 +0530

    Mongoose Setup

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const defaultRouter = require("./routes/defaultRoute");
 const bookRouter = require("./routes/bookRoute");
@@ -9,6 +10,11 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log("Server is up & running!"));
+mongoose
+  .connect("mongodb://127.0.0.1:27017/fsa-b3")
+  .then(() => console.log("DB Connected!"))
+  .catch((err) => console.log(err));
+
 app.use(bodyParser.json());
 
 app.use("/", defaultRouter);
