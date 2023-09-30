8a9b554fb63832218a49b62ff3c281bfd866481d
Sayeed <iamsayeeduddin@gmail.com>
2023-09-30 15:06:47 +0530

    Log Folder Creation

--- utils/appLogger.js
+++ utils/appLogger.js
@@ -1,7 +1,13 @@
 const bunyan = require("bunyan");
 const path = require("path");
+const fs = require("fs");
 
+const dir = path.join(__dirname, "..", "logs");
 const filePath = path.join(__dirname, "..", "logs", "app.log");
+
+if (!fs.existsSync(dir)) {
+  fs.mkdirSync(dir);
+}
 
 const logger = bunyan.createLogger({
   name: "Products",
